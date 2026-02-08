48caf82b3bfd971639d754bf95defec77298f23d
Lucas Lisboa <lucaslisboamotta@gmail.com>
2026-02-08 07:14:36 -0300

    :sparkles: feat: Adicionando classe de conecção a api

---
+++ connections/api/Product.ts
@@ -0,0 +1,38 @@
+export interface IProduct {
+  id: number,
+  name: string,
+  technicalDetails: string,
+  annualValue: string,
+  photos: string[],
+}
+
+class Product {
+    protected baseUrl = 'http://localhost:3001/user/product';
+
+    getAll = async ({search, page, limit}: {search?: string, page: number, limit: number}): Promise<IProduct[]> => {
+        try {
+            const searchParam = search ? `&search=${search}` : '';
+            const response = await fetch(`${this.baseUrl}?page=${page}&limit=${limit}${searchParam}`, {
+                cache: 'force-cache',
+                next: { revalidate: 300 }
+            });
+            return await response.json();
+        } catch {
+            return [];
+        }
+    };
+
+    getById = async (id: string): Promise<IProduct | null> => {
+        try {
+            const response = await fetch(`${this.baseUrl}/${id}`, {
+                cache: 'force-cache',
+                next: { revalidate: 300 }
+            });
+            return await response.json();
+        } catch {
+            return null;
+        }
+    };
+}
+
+export default new Product();
